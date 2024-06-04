680b08d997da6d37f4b3e767599a903c4a59bbad
Loxi <123603348+Loxi9999@users.noreply.github.com>
2024-06-04 13:18:54 +0200

    Add files via upload

---
+++ w_loading/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+game "gta5"
+lua54 'yes'
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+client_script 'client.lua'
+files {
+	'html/index.html',
+	'html/*.js',
+	'html/style.css',
+	'html/*.png'
+}
+loadscreen_manual_shutdown 'yes'
